6cd40a3a4a7195ccc46045c040a7718ce2b700c8
Yun-Chi Hsiao <is0.jimhsiao@gmail.com>
2023-04-17 22:00:07 +1000

    Add: read way function

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import query as q
 def main() -> None:
-	q1 = q.Query("Wilsonasdqwe Hall")
+	q1 = q.Query("Wilson Hall")
 	res = q1.find_way_id()
+	q1.read_way()
 	return None
 
 if __name__ == "__main__":
--- query.py
+++ query.py
@@ -12,6 +12,7 @@ class Query:
 	def __init__(self, building_name):
 		self.building_name = building_name
 		self.way_id = ""
+		self.way = {}
 	
 	# search by the function provided by Boat
 	def find_hyperlink(self) -> str:
@@ -19,19 +20,26 @@ class Query:
 
 	def find_way_id(self) -> str:
 		place = urllib.parse.quote((self.building_name + ", Melbourne, Australia").encode('utf8'))
-		response = requests.get(Query.nominatim_url + "search/{place}?format=json&addressdetails=1&limit=1&polygon_svg=1".format(place=place) + self.map_range)
-		res = json.loads(response.text)
-		if len(res) == 0:
+		
+		try:
+			res = requests.get(Query.nominatim_url + "search/{place}?format=json&addressdetails=1&limit=1&polygon_svg=1".format(place=place) + self.map_range)
+			res = json.loads(res.text)
+			self.way_id = str(res[0]['osm_id'])
+		except:
 			print(self.building_name)
-		elif res[0]['osm_id']:
-			self.way_id = res[0]['osm_id']
-		else:
-			# log error
-			print(self.building_name)
-		# response1 = requests.get(Query.open_street_url + "api/0.6/way/" + str(res[0]['osm_id']))
-		# print(response1.text)
-		# print(self.way_id)
+
 		return self.way_id
 
 	def read_way(self) -> {}:
-		return {}
+		if self.way_id == "": 
+			print("xxx")
+		else:
+			try:
+				print(self.way_id)
+				res = requests.get(Query.open_street_url + "api/0.6/way/" + self.way_id + ".json")
+				self.way = json.loads(res.text)
+				print(self.way)
+			except:
+				print("!!!")
+
+		return self.way
